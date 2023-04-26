1374c0aae594a76b7c78d38ac4310a3dfb6cc449
hacktheegg <jarrahvanritten@gmail.com>
2023-04-27 00:10:36 +1000

    Time for the meatball of code

---
+++ rich-presence.js
@@ -0,0 +1,21 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token, clientId } = require('./config.json');
+const DiscordRPC = require('discord-rpc');
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+	rpc.login({ clientId }).catch(console.error);
+	rpc.on('ready', () => {
+		rpc.setActivity({
+			details: "That fancy code is fun (86 lines)",
+			state: "err: line 253",
+			largeImageKey: "pixelartdiscord",
+			largeImageText: "Discord SpooderMan"
+		});
+	});
+});
+
+client.login(token);
